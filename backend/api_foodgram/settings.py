b0859adf81801553cc4f4228da4fb9c21c32e17b
mishatunikov <michael.tunikov@gmail.com>
2025-04-17 21:07:23 +0300

    feat: Применение переменных окружения для DEBUG, SECRET_KEY

--- backend/api_foodgram/settings.py
+++ backend/api_foodgram/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from config import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,12 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-@v^ad9+is3ntv3cc3p^i26kes%miro@)yk%cj$a%4llm5&n5fk'
-)
-
+SECRET_KEY = config.django_settings.secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config.django_settings.debug
 
 ALLOWED_HOSTS = []
 
