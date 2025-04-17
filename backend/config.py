e35d4044fb99f593046bcf34b7638fc5eb0bcb22
mishatunikov <michael.tunikov@gmail.com>
2025-04-17 20:38:22 +0300

    feat: Создан модуль для загрузки переменных окружения.

---
+++ backend/config.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+
+from environs import Env
+
+
+@dataclass
+class DjangoSettings:
+    """Конфигурационные данные для Django."""
+
+    secret_key: str
+    debug: bool
+
+
+@dataclass
+class PostgreSettings:
+    """Конфигурационные данные для СУБД PostgreSQL."""
+
+    db_user: str
+    db_password: str
+    db_host: str
+    db_name: str
+    db_port: int
+
+
+@dataclass
+class Config:
+    """Конфигурационные данные всего проекта."""
+
+    django_settings: DjangoSettings
+    db: PostgreSettings
+
+
+def load_env() -> Config:
+    env = Env()
+    env.read_env()
+    return Config(
+        DjangoSettings(
+            secret_key=env.str('SECRET_KEY', 'SECRET_KEY'),
+            debug=env.bool('DEBUG'),
+        ),
+        PostgreSettings(
+            db_user=env.str('POSTGRE_USER', 'postgres'),
+            db_password=env.str('POSTGRE_PASSWORD', ''),
+            db_name=env.str('POSGRE_DB', 'postgres'),
+            db_host=env.str('DB_HOST', ''),
+            db_port=env.int('DB_PORT', 5432),
+        ),
+    )
+
+
+config: Config = load_env()
