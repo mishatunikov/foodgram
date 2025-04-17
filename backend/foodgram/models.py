82e4146b854e69de3caa34057d830c91b6a75b22
mishatunikov <michael.tunikov@gmail.com>
2025-04-17 21:42:39 +0300

    feat: Создание модели Tag

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from foodgram.consts import MAX_NAME_FIELD, MAX_SLUG_LENGTH
+
+User = get_user_model()
+
+
+class Tag(models):
+    name = models.CharField(max_length=MAX_NAME_FIELD, verbose_name='Название')
+    slug = models.SlugField(
+        max_length=MAX_SLUG_LENGTH, verbose_name='Идентификатор'
+    )
+
+
+class Recipe(models):
+    pass
