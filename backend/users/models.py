8fd07c67a5d78a26be264e07a828c07e01e4e181
mishatunikov <michael.tunikov@gmail.com>
2025-04-17 21:33:52 +0300

    feat: Значение длин полей вынесены в константы.

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from users.consts import MAX_LENGTH_NAME
+
 
 class CustomUser(AbstractUser):
     email = models.EmailField(verbose_name='Электронная почта')
-    first_name = models.CharField(max_length=150, verbose_name='Имя')
-    last_name = models.CharField(max_length=150, verbose_name='Фамилия')
+    first_name = models.CharField(
+        max_length=MAX_LENGTH_NAME, verbose_name='Имя'
+    )
+    last_name = models.CharField(
+        max_length=MAX_LENGTH_NAME, verbose_name='Фамилия'
+    )
