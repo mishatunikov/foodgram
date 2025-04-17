c85179281f7ae4e5607eac76663f4b0716f803e1
mishatunikov <michael.tunikov@gmail.com>
2025-04-17 20:49:19 +0300

    feat: Переопределена стандартная модель пользователя

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(verbose_name='Электронная почта')
+    first_name = models.CharField(max_length=150, verbose_name='Имя')
+    last_name = models.CharField(max_length=150, verbose_name='Фамилия')
