cbd5da2b62a84f4961af2ff87e2f3221b7ad0aa5
mishatunikov <michael.tunikov@gmail.com>
2025-04-22 23:37:24 +0300

    feat: Регистрация эндпоинта для tags

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from api.views import UserViewSet
+from api.views import UserViewSet, TagViewSet
 
 router = DefaultRouter()
 router.register(prefix='users', viewset=UserViewSet)
+router.register(prefix='tags', viewset=TagViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
