793dbf0de1c8144a739d7c593f7f523667091932
mishatunikov <michael.tunikov@gmail.com>
2025-04-22 18:06:11 +0300

    feat: Обновления штатного Permission на IsAuthenticatedOrReadOnly

--- backend/api_foodgram/settings.py
+++ backend/api_foodgram/settings.py
@@ -132,7 +132,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny'],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
