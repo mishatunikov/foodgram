807f008b02df5ccef7f8e039d6e0060686f8d18a
mishatunikov <michael.tunikov@gmail.com>
2025-04-22 12:05:36 +0300

    feat: Добавление модуля с константами

---
+++ backend/api/consts.py
@@ -0,0 +1,10 @@
+MAX_LENGTH_PASSWORD = 128
+
+# Response messages
+AVATAR_DELETED = 'Аватар успешно удален'
+AVATAR_NOT_INSTALLED = 'В текущем профиле аватар не установлен'
+PASSWORD_UPDATED = 'Пароль успешно обновлен'
+
+# Validation error messages
+NEW_PASSWORD_IS_ALREADY_USED = 'Устанавливаемый пароль совпадает с текущем'
+CURRENT_PASSWORD_IS_WRONG = 'Неверно указан текущий пароль'
