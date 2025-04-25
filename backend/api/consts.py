3b836c0485e88b1e0dd8155af6151eda2e473ebd
mishatunikov <michael.tunikov@gmail.com>
2025-04-25 23:30:16 +0300

    feat: Добавление константы NOT_FOLLOW_SELF

--- backend/api/consts.py
+++ backend/api/consts.py
@@ -8,3 +8,4 @@ PASSWORD_UPDATED = 'Пароль успешно обновлен'
 # Validation error messages
 NEW_PASSWORD_IS_ALREADY_USED = 'Устанавливаемый пароль совпадает с текущем'
 CURRENT_PASSWORD_IS_WRONG = 'Неверно указан текущий пароль'
+NOT_FOLLOW_SELF = 'Подписаться на самого себя нельзя.'
