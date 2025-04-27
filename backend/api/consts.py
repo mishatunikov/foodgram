e0cfccdd38f1a93f39dd7e655d5122ffe53acba2
mishatunikov <michael.tunikov@gmail.com>
2025-04-27 11:38:34 +0300

    feat: Добавление новых констант
    SUBSCRIPTION_DELETED - текстовое сообщение при удалении подписки. RECIPES_LIMIT_PARAM_PATTERN - шаблон передаваемого параметра лимита выводимых рецептов

--- backend/api/consts.py
+++ backend/api/consts.py
@@ -1,11 +1,15 @@
 MAX_LENGTH_PASSWORD = 128
 
 # Response messages
-AVATAR_DELETED = 'Аватар успешно удален'
-AVATAR_NOT_INSTALLED = 'В текущем профиле аватар не установлен'
-PASSWORD_UPDATED = 'Пароль успешно обновлен'
+AVATAR_DELETED = 'Аватар успешно удален.'
+AVATAR_NOT_INSTALLED = 'В текущем профиле аватар не установлен.'
+PASSWORD_UPDATED = 'Пароль успешно обновлен.'
+SUBSCRIPTION_DELETED = 'Подписка успешно отменена.'
 
 # Validation error messages
-NEW_PASSWORD_IS_ALREADY_USED = 'Устанавливаемый пароль совпадает с текущем'
-CURRENT_PASSWORD_IS_WRONG = 'Неверно указан текущий пароль'
+NEW_PASSWORD_IS_ALREADY_USED = 'Устанавливаемый пароль совпадает с текущем.'
+CURRENT_PASSWORD_IS_WRONG = 'Неверно указан текущий пароль.'
 NOT_FOLLOW_SELF = 'Подписаться на самого себя нельзя.'
+
+# Patterns.
+RECIPES_LIMIT_PARAM_PATTERN = r'[1-9]+\d*'
