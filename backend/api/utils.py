62c309886c7c2a2261d4e2edd6c3c0e0594d5056
mishatunikov <michael.tunikov@gmail.com>
2025-05-03 17:04:34 +0300

    feat: Создание функции генерации pdf

---
+++ backend/api/utils.py
@@ -0,0 +1,46 @@
+import io
+from typing import Union
+
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
+
+from api import consts
+
+
+def create_pdf(
+    data: list, filename: Union[str, io.BytesIO], header: str
+) -> None:
+    """
+    Создает pdf файл для на основе входных данных.
+
+    :param data: Список, каждый элемент которого - новая строка
+    :param filename: Имя файла или объект буффера
+    :param header: Заголовок файла
+    """
+    pdf = canvas.Canvas(filename=filename, pagesize=A4)
+    pdfmetrics.registerFont(TTFont('DejaVu-Bold', 'DejaVuSans-Bold.ttf'))
+    pdfmetrics.registerFont(TTFont('DejaVu', 'DejaVuSans.ttf'))
+    pdf.setFont('DejaVu-Bold', 16)
+    width, height = A4
+    y = height - consts.TOP_MARGIN
+    x = consts.LEFT_MARGIN
+    pdf.drawString(
+        x=x,
+        y=y,
+        text=header,
+    )
+    y -= consts.MARGIN_AFTER_HEADER
+    pdf.setFont('DejaVu', 14)
+
+    for line in data:
+        if y < consts.BOTTOM:
+            pdf.showPage()
+            pdf.setFont('DejaVu', 14)
+            y = height - consts.TOP_MARGIN
+        pdf.drawString(x, y, line)
+        y -= consts.LEADING
+
+    pdf.showPage()
+    pdf.save()
