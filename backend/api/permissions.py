a8184c598c268b92e3594c514eca36f2f2ce1d16
mishatunikov <michael.tunikov@gmail.com>
2025-05-04 12:21:26 +0300

    feat: Создание пермишина IsAdminOrOwnerOrReadOnly

---
+++ backend/api/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAdminOrOwnerOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS or request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_staff
+            or obj.author == request.user
+        )
