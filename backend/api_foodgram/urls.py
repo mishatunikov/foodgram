9ca7f9f21070b855eee300fe09b935b7f6dfa947
mishatunikov <michael.tunikov@gmail.com>
2025-05-05 19:37:54 +0300

    feat: Добавление эндпоинта для короткой ссылки

--- backend/api_foodgram/urls.py
+++ backend/api_foodgram/urls.py
@@ -3,9 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from api_foodgram.views import redirect_to_recipe
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('s/<str:link_id>/', redirect_to_recipe),
 ]
 
 if settings.DEBUG:
