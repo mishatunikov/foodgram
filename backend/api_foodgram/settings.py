a25a0f6289e3bb83835615a4307b130c2a9577c4
mishatunikov <michael.tunikov@gmail.com>
2025-05-06 19:42:11 +0300

    feat: Добавления ALLOWED_HOSTS

--- backend/api_foodgram/settings.py
+++ backend/api_foodgram/settings.py
@@ -14,7 +14,12 @@ SECRET_KEY = config.django_settings.secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config.django_settings.debug
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    config.host.domain_name,
+    config.host.host_ip,
+]
 
 
 # Application definition
@@ -79,10 +84,20 @@ WSGI_APPLICATION = 'api_foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'data/db.sqlite3',
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config.db.db_name,
+#         'USER': config.db.db_user,
+#         'PASSWORD': config.db.db_password,
+#         'HOST': config.db.db_host,
+#         'PORT': config.db.db_port,
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -121,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / 'collected_static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -130,7 +145,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.CustomUser'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny'],
