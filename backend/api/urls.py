343f3010fc3ca03c8415aeba4f0d521e64711a7c
mishatunikov <michael.tunikov@gmail.com>
2025-05-08 14:27:57 +0300

    feat: Переименование роутера для поддержки версионирования

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,16 +3,16 @@ from rest_framework.routers import DefaultRouter
 
 from api.views import IngredientViewSet, RecipeViewSet, TagViewSet, UserViewSet
 
-router = DefaultRouter()
-router.register(prefix='users', viewset=UserViewSet, basename='users')
-router.register(prefix='tags', viewset=TagViewSet, basename='tags')
-router.register(prefix='recipes', viewset=RecipeViewSet, basename='recipes')
-router.register(
+router_v1 = DefaultRouter()
+router_v1.register(prefix='users', viewset=UserViewSet, basename='users')
+router_v1.register(prefix='tags', viewset=TagViewSet, basename='tags')
+router_v1.register(prefix='recipes', viewset=RecipeViewSet, basename='recipes')
+router_v1.register(
     prefix='ingredients', viewset=IngredientViewSet, basename='ingredients'
 )
 
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router_v1.urls)),
     path('auth/', include('djoser.urls.authtoken')),
 ]
