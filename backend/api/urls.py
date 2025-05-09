a892c01fb46f1c9c1918a7ed8fd85a704d5c7599
mishatunikov <michael.tunikov@gmail.com>
2025-05-09 20:34:43 +0300

    feat: Добавление путей для документации

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import include, path
+from django.views.generic import TemplateView
+from django.views.static import serve
 from rest_framework.routers import DefaultRouter
 
 from api.views import IngredientViewSet, RecipeViewSet, TagViewSet, UserViewSet
+from api_foodgram import settings
 
 router_v1 = DefaultRouter()
 router_v1.register(prefix='users', viewset=UserViewSet, basename='users')
@@ -11,8 +14,19 @@ router_v1.register(
     prefix='ingredients', viewset=IngredientViewSet, basename='ingredients'
 )
 
-
+docs_url = [
+    path('', TemplateView.as_view(template_name='docs/redoc.html')),
+    path(
+        'openapi-schema.yml',
+        serve,
+        kwargs={
+            'path': 'docs/openapi-schema.yml',
+            'document_root': settings.BASE_DIR / 'templates',
+        },
+    ),
+]
 urlpatterns = [
+    path('docs/', include(docs_url)),
     path('', include(router_v1.urls)),
     path('auth/', include('djoser.urls.authtoken')),
 ]
